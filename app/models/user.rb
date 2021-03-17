5a341c3b0745cfc697f3871e36e558991e876993
Aline Thome <athomewilliams@gmail.com>
2021-03-16 22:23:59 -0600

    Add password interface to the user model
    Because we are only storing the password hash, we need a way to create a
    user with a plaintext password and a way of comparing the stored hash
    with a given string. This is accomplished through BCrypts built-in
    methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#password=' do 
+    let (:user_with_pass) { User.new(email: 'user@email.com',
+                                     password: 'password')}
+    it 'sets the password' do 
+      expect(user_with_pass.password).to eq("password")
+    end
+  end
+
+  describe 'is_password?' do 
+    let (:user_with_pass) { User.new(email: 'user@email.com',
+                                     password: 'password')}
+
+    context 'given the wrong password' do 
+      it 'returns false' do 
+        expect(user_with_pass.is_password?("Password")).to be false
+        expect(user_with_pass.is_password?("pass")).to be false
+        expect(user_with_pass.is_password?("passrord")).to be false
+      end 
+    end
+
+    context 'given an empty password' do 
+      it 'returns false' do 
+        expect(user_with_pass.is_password?("")).to be false
+      end
+    end
+
+    context 'given the correct password' do 
+      it 'return true' do 
+        expect(user_with_pass.is_password?("password")).to be true
+      end
+    end
+  end
 end
