b7ac2d62d24452c091728a8d1e2bb8bc5f8869a9
Aline Thome <athomewilliams@gmail.com>
2021-03-17 17:03:14 -0600

    Configure api routes for users
    This will allow us to create users and retrieve user information
    via AJAX so we can build a single page app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
+
+  namespace :api, defaults: { format: :json } do
+    resources :user, only: [:create, :update, :destroy, :show]
+  end
 end
