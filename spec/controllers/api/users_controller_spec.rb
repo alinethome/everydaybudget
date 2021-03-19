5a9629191f051cffee0402ecbbd80c6262865b08
Aline Thome <athomewilliams@gmail.com>
2021-03-18 19:15:28 -0600

    Implement api users controller get create method
    This will allow our app to create a new user using api.

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
+  describe 'POST #create' do 
+    context 'with valid parameters' do 
+      it 'renders the newly created user as json' do
+        post :create, 
+          params: { user: FactoryBot.attributes_for(:user) }, 
+          format: :json
+        expect(response).to render_template(:show)
+      end
 
+      it 'creates a new user' do 
+        expect{
+          post :create, params: { user: FactoryBot.attributes_for(:user) },
+          format: :json
+        }.to change(User, :count).by(1)
+      end
+    end
+
+    context 'with invalid parameters' do 
+      it 'does not create a new user' do 
+        expect{
+          post :create, params: { user: { email: '', 
+                                          password: '' } }, format: :json
+        }.to_not change(User, :count)
+      end
+
+      it 'returns an unprocessable entity status' do
+        post :create, 
+          params: { user: { email: '', password: '' }},
+          format: :json
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a json response' do 
+        post :create, 
+          params: { user: { email: '', password: '' }},
+          format: :json
+        expect(response.header['Content-Type'].should include 'application/json')
+      end
+    end
+  end
 end
