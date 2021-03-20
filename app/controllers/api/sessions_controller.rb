a719433e8aca4472175cf280b4386911838c784e
Aline Thome <athomewilliams@gmail.com>
2021-03-19 18:23:09 -0600

    Add create action for api sessions controller
    This will give us a controller route to direct our ajax requests to so
    that the user can log in without requiring a separate page.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,18 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
+
+    if @user
+      sign_in!(@user)
+      render 'api/users/show'
+    else
+      render json: ["Wrong email/password combination"], status: 401
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Api::SessionsController, type: :controller do
+  describe 'POST #create' do
+    context 'with valid email and password' do
+      before(:each) do 
+        @attributes_for_user = FactoryBot.attributes_for(:user)
+        @user = User.create(@attributes_for_user) 
+        post :create,
+          params: { user: @attributes_for_user },
+          format: :json
+      end
 
+      after(:each) do
+        @user.destroy
+      end
+
+      it 'signs the user in' do
+        expect(session[:session_token]).to eq(@user.session_token)
+      end
+
+      it 'renders the user as json' do 
+        expect(response).to render_template(:show)
+      end
+    end
+
+    context 'with invalid email and password' do
+      before(:each) do
+        @attributes_for_user = { email: '', password: ''} 
+        session[:session_token] = nil
+      end
+
+      it 'does not sign the user in' do
+        post :create,
+          params: { user: @attributes_for_user },
+          format: :json
+
+        expect(session[:session_token]).to be_nil
+      end
+
+      it 'renders a json error hash' do
+        post :create,
+          params: { user: @attributes_for_user }
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      it 'returns an unauthorized status code' do
+        post :create,
+          params: { user: @attributes_for_user }
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
 end
