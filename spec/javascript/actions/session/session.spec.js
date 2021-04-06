9ec73ce3da3ae5b856c066b5b1746ffd5ab2344f
Aline Thome <athomewilliams@gmail.com>
2021-04-06 18:40:21 -0500

    Fix the createNewUser tests
    The previous implementation of the postUser tests weren't truly unit
    tests because they failed to mock the api functions the function being
    tested relied on. Instead they mocked the ajax call. The change to
    postUser broke the previous test since the token couldn't be stubbed in.
    This new implementation mocks the api function directly to fix it.

--- spec/javascript/actions/session/session.spec.js
+++ spec/javascript/actions/session/session.spec.js
@@ -1,18 +1,19 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { createNewUser, RECEIVE_CURRENT_USER } from 'frontend/actions/session.js';
-import $ from 'jquery';
+import * as API from 'frontend/utils/session/api_util.js';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const id = 1;
-$.ajax = jest.fn().mockImplementation(() => {
-    const fakeResponse = {
-        id,
-        email: "anemail@email.com"
-    };
-    return Promise.resolve(fakeResponse);
-});
+const postUser = jest.spyOn(API, 'postUser')
+    .mockImplementation(() => {
+        const fakeResponse = {
+            id,
+            email: "anemail@email.com"
+        };
+        return Promise.resolve(fakeResponse);
+    });
 
 describe('createNewUser', () => {
     afterEach(() => {
