4e151d0a7cbb6b7455d8429025c1c548fb38fae9
Aline Thome <athomewilliams@gmail.com>
2021-04-06 16:32:17 -0500

    Create a test for redirect after sign-up
    So we know that the react front end is successfully redirecting the user
    back to the root once they've been signed on and logged in.

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -5,4 +5,15 @@ feature 'the signup process', js: true do
     visit '/#/register'
     expect(page).to have_content 'Sign Up for EverydayBudget'
   end
+
+  scenario 'redirects a user to the home page after a successful signup' do 
+    user = FactoryBot.attributes_for(:user)
+
+    visit '/#/register'
+    fill_in'Email:', with: user[:email]
+    fill_in'Password:', with: user[:password]
+    click_button('Sign up!')
+
+    expect(current_path).to eq('/');
+  end
 end
