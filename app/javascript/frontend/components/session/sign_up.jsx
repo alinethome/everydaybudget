81cd629f059dc498c6b852a1540ae3a681b0c4cb
Aline Thome <athomewilliams@gmail.com>
2021-04-06 14:00:47 -0500

    Modify the signup form to work with our actions
    Previously the form was unable to handle any data and was just there for
    testing. Now, input has been controlled and the a submit handler has
    been attached so that the new user is created on the backend and the
    returned user is logged in.

--- app/javascript/frontend/components/session/sign_up.jsx
+++ app/javascript/frontend/components/session/sign_up.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
 
 class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(type) {
+        return (e) => {
+            this.setState({ [type]: e.target.value });
+        };
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createNewUser(this.state)
+            .then(() => this.props.history.push('/'), 
+            (error) => console.log(error));
+    }
+
+    getToken() {
+        return document.elementsByName('csrf-token')[0].content;
+    }
+
     render() {
         return (
             <form>
                 <h1>Sign Up for EverydayBudget</h1>
                 <label>Email:
-                    <input type="text" />
+                    <input type="text" 
+                        value={ this.state.email }
+                        onChange={ this.handleInput('email') }/>
                 </label>
                 <label>Password:
-                    <input type="password" />
+                    <input type="password"
+                        value={ this.state.password }
+                        onChange={ this.handleInput('password') }/>
                 </label>
-                <input type="submit" value="Sign up!"/>
+                <input type="submit" 
+                    value="Sign up!"
+                    onClick={ this.handleSubmit } />
             </form>
         );
     }
