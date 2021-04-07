6c6228826b3ca7304a9c2e67f8d6464b97469ad8
Aline Thome <athomewilliams@gmail.com>
2021-04-07 16:58:37 -0500

    Implement the logout action creator
    This will allow our redux structure to handle successful logouts on the
    frontend.

--- app/javascript/frontend/actions/session.js
+++ app/javascript/frontend/actions/session.js
@@ -1,15 +1,25 @@
-import { postUser } from '../utils/session/api_util.js';
+import { postUser, deleteSession } from '../utils/session/api_util.js';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 
 const receiveCurrentUser = (user) => ({
     type: RECEIVE_CURRENT_USER,
     user
 });
 
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+});
+
 export const createNewUser = (newUser) => (dispatch) => {
     return postUser(newUser)
         .then((createdUser) => dispatch(receiveCurrentUser(createdUser)));
+};
+
+export const logout = () => (dispatch) => {
+    return deleteSession()
+        .then(() => dispatch(logoutCurrentUser()))
 };
 
 
--- spec/javascript/actions/session/session.spec.js
+++ spec/javascript/actions/session/session.spec.js
@@ -1,17 +1,27 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { createNewUser, RECEIVE_CURRENT_USER } from 'frontend/actions/session.js';
+import { 
+    createNewUser, RECEIVE_CURRENT_USER,
+    logout, LOGOUT_CURRENT_USER
+} from 'frontend/actions/session.js';
 import * as API from 'frontend/utils/session/api_util.js';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const id = 1;
+
 const postUser = jest.spyOn(API, 'postUser')
     .mockImplementation(() => {
         const fakeResponse = {
             id,
             email: "anemail@email.com"
         };
+        return Promise.resolve(fakeResponse);
+    });
+
+const deleteSession = jest.spyOn(API, 'deleteSession')
+    .mockImplementation(() => {
+        const fakeResponse = "";
         return Promise.resolve(fakeResponse);
     });
 
@@ -30,6 +40,31 @@ describe('createNewUser', () => {
 
         return store.dispatch(createNewUser(user)).then(() => 
             expect(store.getActions()).toEqual(expectedActions)
+        );
+    });
+});
+
+describe('logout', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('the store should receive an action to logout the current user', () => {
+        const store = mockStore({ session: { current_user: {} } });
+        const expectedActions = [{ 
+            type: LOGOUT_CURRENT_USER
+        }];
+
+        return store.dispatch(logout()).then(() => 
+            expect(store.getActions()).toEqual(expectedActions)
+        );
+    });
+
+    test('should call deleteUser', () => {
+        const store = mockStore({ session: { current_user: {} } });
+
+        return store.dispatch(logout()).then(() => 
+            expect(deleteSession.mock.calls).toBeTruthy()
         );
     });
 });
