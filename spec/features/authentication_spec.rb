7822c93228b242382690124d8ab56e7494e65005
Aline Thome <athomewilliams@gmail.com>
2021-04-15 13:07:36 -0500

    Add feature tests for the login form

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -20,7 +20,7 @@ feature 'the signup process', js: true do
     visit '/#/signup'
     fill_in'Email:', with: user[:email]
     fill_in'Password:', with: user[:password]
-    click_button('Sign up!')
+    click_button('Sign Up!')
 
     expect(react_path).to eq('/signup');
   end
@@ -31,7 +31,7 @@ feature 'the signup process', js: true do
     visit '/#/signup'
     fill_in'Email:', with: user[:email]
     fill_in'Password:', with: user[:password]
-    click_button('Sign up!')
+    click_button('Sign Up!')
 
     expect(page).to have_content user[:email]
   end
@@ -42,7 +42,43 @@ feature 'the signup process', js: true do
     visit '/#/signup'
     fill_in'Email:', with: user[:email]
     fill_in'Password:', with: user[:password]
-    click_button('Sign up!')
+    click_button('Sign Up!')
+
+    expect(page).to have_content 'Logout'
+  end
+end
+
+feature 'the log in process', js: true do
+  before do
+      ActionController::Base.allow_forgery_protection = true
+
+      @user = FactoryBot.create(:user)
+  end
+
+  after do
+      ActionController::Base.allow_forgery_protection = false
+      @user.destroy
+  end
+
+  scenario 'has a log in page' do
+    visit '/#/login'
+    expect(page).to have_content 'Log In'
+  end
+
+  scenario 'displays the user\'s email after a successful login' do
+    visit '/#/login'
+    fill_in'Email:', with: @user.email
+    fill_in'Password:', with: @user.password
+    click_button('Log In!')
+
+    expect(page).to have_content @user.email
+  end
+
+  scenario 'display a logout button after a successful login' do
+    visit '/#/login'
+    fill_in'Email:', with: @user.email
+    fill_in'Password:', with: @user.password
+    click_button('Log In!')
 
     expect(page).to have_content 'Logout'
   end
