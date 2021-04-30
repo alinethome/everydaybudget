ad4fd465ea623935fb64334afa5c1061f7cae5d2
Aline Thome <athomewilliams@gmail.com>
2021-04-30 15:17:30 -0500

    Add validations to the budget item model

--- app/models/budget_item.rb
+++ app/models/budget_item.rb
@@ -1,2 +1,4 @@
 class BudgetItem < ApplicationRecord
+  validates :user_id, :name, :type, :is_recurring, :amount, :date, presence: true
+  validates :recur_unit, inclusion: { in: ["days", "weeks", "months"] }
 end
--- spec/models/budget_item_spec.rb
+++ spec/models/budget_item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BudgetItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:is_recurring) }
+    it { should validate_presence_of(:amount) }
+    it { should validate_presence_of(:date) }
+    it { should validate_inclusion_of(:recur_unit)
+         .in_array(["months", "weeks", "days"])}
+  end
 end
