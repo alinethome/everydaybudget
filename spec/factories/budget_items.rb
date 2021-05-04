fed2f1a5ddba3d33e63884c1e3146aaf084d44a7
Aline Thome <athomewilliams@gmail.com>
2021-05-04 17:49:48 -0500

    Add budget item factory

--- spec/factories/budget_items.rb
+++ spec/factories/budget_items.rb
@@ -1,5 +1,26 @@
 FactoryBot.define do
   factory :budget_item do
+    name { "Budget Item" }
+    amount { 10.00 }
+    date { DateTime.now() }
+    type { "expense" }
+    user
+
+    trait :expense do 
+      type { "expense" }
+    end
+
+    trait :income do
+      type { "income" }
+    end
+
+    trait :recurring do
+      is_recurring { true }
+    end
+
+    trait :not_recurring do
+      is_recurring { false }
+    end
     
   end
 end
