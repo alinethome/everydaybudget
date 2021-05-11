540044a459dfd7bedffb43d2b116b391b58680f5
Aline Thome <athomewilliams@gmail.com>
2021-05-11 12:53:14 -0500

    Fix the tests to use a helper for examples
    Previously, examples were generated by creating a new DateTime object
    directly in the test block. This made it hard to parse out what the date
    in question was and why the the result of the test ought to be what it
    is. Using the helpers to set start and end dates makes the tests shorter
    and easier to understand.

--- spec/models/months_unit_item_spec.rb
+++ spec/models/months_unit_item_spec.rb
@@ -25,9 +25,8 @@ RSpec.describe MonthsUnitItem, type: :model do
           context 'if the current month is longer than its start day' do
             it 'returns its start_date\'s day' do
               start_day = 30
-              start_date = DateTime.new(@current_year, 3, start_day,
-                                        3, 33, 0) # March 31, 2021
-              expense_months_item.start_date = start_date
+              set_start_date(item: expense_months_item, year: @current_year,
+                             month: 3, day: start_day)
               expense_months_item.recur_period = 2 #months
 
               expect(expense_months_item.
@@ -38,14 +37,14 @@ RSpec.describe MonthsUnitItem, type: :model do
           context 'if the current month is shorter than its start day' do
             it 'returns the last day of the current month' do
               travel_to DateTime.new(2021, 2, 13, 7, 30, 0) # february
+              days_in_february = 28
 
-              start_date = DateTime.new(@current_year, 1, 30,
-                                  3, 33, 0) # January 30th, 2021
-              expense_months_item.start_date = start_date
-              expense_months_item.recur_period = 1 #month
+              set_start_date(item: expense_months_item, year: @current_year,
+                             month: 1, day: 30)
+              expense_months_item.recur_period = 1 
 
               expect(expense_months_item.
-                     first_instance_this_month).to eq(28)
+                     first_instance_this_month).to eq(days_in_february)
             end
           end
         end
@@ -53,14 +52,10 @@ RSpec.describe MonthsUnitItem, type: :model do
         context 'if it has an end date' do
           context 'if the end date is before the current month' do
             it 'returns nil' do
-              travel_to DateTime.new(2021, 2, 13, 7, 30, 0) # february
-
-              start_date = DateTime.new(@current_year - 1, 1, 30,
-                                  3, 33, 0) # January 30th, 2020
-              end_date = DateTime.new(@current_year, 3, 1,
-                                     10, 43, 14) # March 1st, 2021
-              expense_months_item.start_date = start_date
-              expense_months_item.end_date = end_date
+              set_start_date(item: expense_months_item, year: @current_year,
+                             month: 1, day: 30)
+              set_end_date(item: expense_months_item, year: @current_year,
+                             month: 3, day: 1)
               expense_months_item.recur_period = 1 #month
 
               expect(expense_months_item.
@@ -71,13 +66,13 @@ RSpec.describe MonthsUnitItem, type: :model do
           context 'if the end date is in the current month' do
             context 'if the end date is before its start day' do
               it 'returns nil' do
-                start_date = DateTime.new(@current_year - 1, 7, 4,
-                                          14, 0, 0) # July 4th, 2020
-                end_date = DateTime.new(@current_year, @current_month, 3, 
-                                        3, 0, 0) # May 3rd, 2021
+                set_start_date(item: expense_months_item, 
+                               year: @current_year - 1, month: 7,
+                               day: 4)
+                set_end_date(item: expense_months_item, 
+                               year: @current_year, month: @current_month,
+                               day: 3)
                 expense_months_item.recur_period = 2 # months
-                expense_months_item.start_date = start_date 
-                expense_months_item.end_date = end_date 
 
                 expect(expense_months_item.
                        first_instance_this_month).to be_nil
@@ -87,13 +82,13 @@ RSpec.describe MonthsUnitItem, type: :model do
             context 'if the end date is after it\'s start day' do
               it 'returns its start day' do
                 start_day = 4
-                start_date = DateTime.new(@current_year - 1, 7, start_day,
-                                          14, 0, 0) # July 4th, 2020
-                end_date = DateTime.new(@current_year, @current_month, 5, 
-                                        3, 0, 0) # May 5rd, 2021
+                set_start_date(item: expense_months_item, 
+                               year: @current_year - 1, month: 7,
+                               day: start_day)
+                set_end_date(item: expense_months_item, 
+                               year: @current_year, month: @current_month,
+                               day: start_day + 1)
                 expense_months_item.recur_period = 2 # months
-                expense_months_item.start_date = start_date 
-                expense_months_item.end_date = end_date 
 
                 expect(expense_months_item.
                        first_instance_this_month).to eq(start_day)
@@ -106,9 +101,8 @@ RSpec.describe MonthsUnitItem, type: :model do
       context 'if it won\'t recur in the given month' do 
         it 'returns nil' do
           start_day = 31
-          start_date = DateTime.new(@current_year, 1, start_day,
-                                    3, 33, 0) # January 31, 2021
-          expense_months_item.start_date = start_date
+          set_start_date(item: expense_months_item, year: @current_year, 
+                         month: 1, day: start_day)
           expense_months_item.recur_period = 3 #months
 
           expect(expense_months_item.first_instance_this_month).to be_nil
@@ -122,27 +116,26 @@ RSpec.describe MonthsUnitItem, type: :model do
           context 'if the current month is longer than its start day' do
             it 'returns its start_date\'s day' do
               start_day = 31
-              start_date = DateTime.new(@current_year, 3, start_day,
-                                        3, 33, 0) # March 31, 2021
-              income_months_item.start_date = start_date
+              set_start_date(item: income_months_item, year: @current_year,
+                             month: 3, day: start_day) 
               income_months_item.recur_period = 2 #months
 
               expect(income_months_item.
-                     first_instance_this_month).to eq(31)
+                     first_instance_this_month).to eq(start_day)
             end
           end
 
           context 'if the current month is shorter than its start day' do
             it 'returns the last day of the current month' do
               travel_to DateTime.new(2021, 2, 13, 7, 30, 0) # february
+              days_in_february = 28
 
-              start_date = DateTime.new(@current_year, 1, 30,
-                                  3, 33, 0) # January 30th, 2021
-              income_months_item.start_date = start_date
+              set_start_date(item: income_months_item, year: @current_year,
+                             month: 1, day: 30)
               income_months_item.recur_period = 1 #month
 
               expect(income_months_item.
-                     first_instance_this_month).to eq(28)
+                     first_instance_this_month).to eq(days_in_february)
             end
           end
         end
@@ -150,14 +143,11 @@ RSpec.describe MonthsUnitItem, type: :model do
         context 'if it has an end date' do
           context 'if the end date is before the current month' do
             it 'returns nil' do
-              travel_to DateTime.new(2021, 2, 13, 7, 30, 0) # february
-
-              start_date = DateTime.new(@current_year - 1, 1, 30,
-                                  3, 33, 0) # January 30th, 2020
-              end_date = DateTime.new(@current_year, 3, 1,
-                                     10, 43, 14) # March 1st, 2021
-              income_months_item.start_date = start_date
-              income_months_item.end_date = end_date
+              set_start_date(item: income_months_item, 
+                             year: @current_year - 1, month: 1, 
+                             day: 30)
+              set_end_date(item: income_months_item, year: @current_year, 
+                           month: 3, day: 31)
               income_months_item.recur_period = 1 #month
 
               expect(income_months_item.
@@ -168,29 +158,27 @@ RSpec.describe MonthsUnitItem, type: :model do
           context 'if the end date is in the current month' do
             context 'if the end date is before its start day' do
               it 'returns nil' do
-                start_date = DateTime.new(@current_year - 1, 7, 4,
-                                          14, 0, 0) # July 4th, 2020
-                end_date = DateTime.new(@current_year, @current_month, 3, 
-                                        3, 0, 0) # May 3rd, 2021
+                set_start_date(item: income_months_item, 
+                               year: @current_year - 1, month: 7, day: 4)
+                set_end_date(item: income_months_item, 
+                             year: @current_year, month: @current_month,
+                             day: 3)
                 income_months_item.recur_period = 2 # months
-                income_months_item.start_date = start_date 
-                income_months_item.end_date = end_date 
 
                 expect(income_months_item.
                        first_instance_this_month).to be_nil
               end
             end
 
-            context 'if the end date is after it\'s start day' do
+            context 'if the end date is after its start day' do
               it 'returns its start day' do
                 start_day = 4
-                start_date = DateTime.new(@current_year - 1, 7, start_day,
-                                          14, 0, 0) # July 4th, 2020
-                end_date = DateTime.new(@current_year, @current_month, 5, 
-                                        3, 0, 0) # May 5rd, 2021
+                set_start_date(item: income_months_item, 
+                               year: @current_year - 1, month: 7,
+                               day: start_day)
+                set_end_date(item: income_months_item, year: @current_year,
+                             month: @current_month, day: 5)
                 income_months_item.recur_period = 2 # months
-                income_months_item.start_date = start_date 
-                income_months_item.end_date = end_date 
 
                 expect(income_months_item.
                        first_instance_this_month).to eq(start_day)
@@ -203,9 +191,8 @@ RSpec.describe MonthsUnitItem, type: :model do
       context 'if it won\'t recur in the given month' do 
         it 'returns nil' do
           start_day = 31
-          start_date = DateTime.new(@current_year, 1, start_day,
-                                    3, 33, 0) # January 31, 2021
-          income_months_item.start_date = start_date
+          set_start_date(item: income_months_item, year: @current_year,
+                         month: 1, day: start_day)
           income_months_item.recur_period = 3 #months
 
           expect(income_months_item.first_instance_this_month).to be_nil
@@ -215,3 +202,23 @@ RSpec.describe MonthsUnitItem, type: :model do
   end
 end
 
+
+def set_start_date(item:, year:, month:, day:, **time)
+  time[:hour] = time[:hour] || rand(24)
+  time[:minute] = time[:minute] || rand(60)
+  time[:second] = time[:second] || rand(60)
+
+  date = DateTime.new(year, month, day, 
+                      time[:hour], time[:minute], time[:second])
+  item.start_date = date
+end
+
+def set_end_date(item:, year:, month:, day:, **time)
+  time[:hour] = time[:hour] || rand(24)
+  time[:minute] = time[:minute] || rand(60)
+  time[:second] = time[:second] || rand(60)
+
+  date = DateTime.new(year, month, day, 
+                      time[:hour], time[:minute], time[:second])
+  item.end_date = date
+end
