0945a405f53eae806e74827fd2cfab8457366c16
Aline Thome <athomewilliams@gmail.com>
2021-05-18 20:19:35 -0500

    Add an index route for recurring items
    This will allow the frontend to request information about all of the
    recurring items associated to the current user through the API.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update, :destroy, :show] do
-      resources :recurring_items, only: [:create]
+      resources :recurring_items, only: [:create, :index]
     end
 
     resources :recurring_items, only: [:update, :destroy, :show]
