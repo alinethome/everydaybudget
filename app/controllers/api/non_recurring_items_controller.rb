27bb8877068b5c14c81062dee24ab131c279d5bb
Aline Thome <athomewilliams@gmail.com>
2021-05-18 21:15:06 -0500

    Generate the NonRecurringItemsController

---
+++ app/controllers/api/non_recurring_items_controller.rb
@@ -0,0 +1,2 @@
+class Api::NonRecurringItemsController < ApplicationController
+end
---
+++ spec/controllers/api/non_recurring_items_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Api::NonRecurringItemsController, type: :controller do
+
+end
