142cd3622102c3bac7d6826c0b8c8c2345f94f2f
Aline Thome <athomewilliams@gmail.com>
2021-05-21 18:34:08 -0500

    Fix routes so items aren't nested within users
    Since only one user is going to be logged at a time and all items are
    going to be created for that particular user, there is no need to have
    the create and index routes for recurring and non-recurring items nested
    under user. There is no ambiguity as to which user they should belong
    to.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   root to: 'static_pages#index'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :update, :destroy, :show] do
-      resources :recurring_items, only: [:create, :index]
-      resources :non_recurring_items, only: [:create, :index]
-    end
-
-    resources :recurring_items, only: [:update, :destroy, :show]
-    resources :non_recurring_items, only: [:update, :destroy, :show]
+    resources :users, only: [:create, :update, :destroy, :show]
+    resources :recurring_items, except: [:new, :edit]
+    resources :non_recurring_items, except: [:new, :edit]
     resource :session, only: [:create, :destroy]
     resource :budget, only: [:show]
   end
