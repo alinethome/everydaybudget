b4a1b28bbb5086d47a9114b885d98d65fe22f06c
Aline Thome <athomewilliams@gmail.com>
2021-05-25 16:32:00 -0500

    Fix recurring item factory
    Previously it would default end_date to undefined if it wasn't provided
    with the attribute object. Null seems more descriptive and the
    controller knows what to do with it.

--- spec/javascript/factories/recurring_item.js
+++ spec/javascript/factories/recurring_item.js
@@ -3,7 +3,7 @@ function RecurringItem(attributes) {
     this.name = attributes.name || "a recurring item";
     this.type = attributes.type || "income";
     this.start_date = attributes.start_date || new Date();
-    this.end_date = attributes.end_date;
+    this.end_date = attributes.end_date || null;
     this.amount = attributes.amount || 1000;
     this.recur_period = attributes.recur_period || 1;
     this.recur_unit_type = attributes.recur_unit_type || "MonthsUnitItem";
