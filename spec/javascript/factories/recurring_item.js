8405bbcba92db7e17bb9c9af6e69c13a406debb2
Aline Thome <athomewilliams@gmail.com>
2021-05-26 16:58:26 -0500

    Fix recurringItem factory
    The syntax was using function syntax, as opposed to class syntax. This
    has been fixed to match the NonRecurringItem factory.

--- spec/javascript/factories/recurring_item.js
+++ spec/javascript/factories/recurring_item.js
@@ -1,14 +1,16 @@
-function RecurringItem(attributes) {
-    this.id = attributes.id || 1;
-    this.name = attributes.name || "a recurring item";
-    this.type = attributes.type || "income";
-    this.start_date = attributes.start_date || new Date();
-    this.end_date = attributes.end_date || null;
-    this.amount = attributes.amount || 1000;
-    this.recur_period = attributes.recur_period || 1;
-    this.recur_unit_type = attributes.recur_unit_type || "MonthsUnitItem";
-    this.month_instances = attributes.month_instances || 
-        [this.start_date.getDate()];
+class RecurringItem {
+    constructor(attributes) {
+        this.id = attributes.id || 1;
+        this.name = attributes.name || "a recurring item";
+        this.type = attributes.type || "income";
+        this.start_date = attributes.start_date || new Date();
+        this.end_date = attributes.end_date || null;
+        this.amount = attributes.amount || 1000;
+        this.recur_period = attributes.recur_period || 1;
+        this.recur_unit_type = attributes.recur_unit_type || "MonthsUnitItem";
+        this.month_instances = attributes.month_instances || 
+            [this.start_date.getDate()];
+    }
 }
 
 export default RecurringItem;
