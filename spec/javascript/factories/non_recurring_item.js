e0d6fb01ca1622f2b671cb6ac5f81bbf1347aaf8
Aline Thome <athomewilliams@gmail.com>
2021-06-09 16:29:28 -0500

    Fix non-recurring item factory
    The non-recurring item factory was checking for a start date in the
    options hash and initialising with a new Date object otherwise.
    start_date is not a field for non-recurring items, however, so it was
    alwayys defaulting to the new object. This has now been fixed.

--- spec/javascript/factories/non_recurring_item.js
+++ spec/javascript/factories/non_recurring_item.js
@@ -3,7 +3,7 @@ class NonRecurringItem {
         this.id = attributes.id || 1;
         this.name = attributes.name || "a recurring item";
         this.type = attributes.type || "expense";
-        this.date = attributes.start_date || new Date();
+        this.date = attributes.date || new Date();
         this.amount = attributes.amount || 50;
         this.month_instances = attributes.month_instances || 
             [this.date.getDate()];
