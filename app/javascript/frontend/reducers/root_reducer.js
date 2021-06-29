2767bf60e9d4c0d86144de35800b414b702498e9
Aline Thome <athomewilliams@gmail.com>
2021-06-29 13:06:55 -0500

    Add display reducer to the root reducer

--- app/javascript/frontend/reducers/root_reducer.js
+++ app/javascript/frontend/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import sessionReducer from './session_reducer.js';
 import entitiesReducer from './entities_reducer.js';
 import budgetReducer from './budget_reducer.js';
 import errorsReducer from './errors_reducer.js';
+import displayReducer from './display_reducer.js';
 
 export default combineReducers({
     session: sessionReducer,
     entities: entitiesReducer,
     budget: budgetReducer,
-    errors: errorsReducer
+    errors: errorsReducer,
+    display: displayReducer
 });
 
